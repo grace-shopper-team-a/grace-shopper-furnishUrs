c633660300152e0aa4cfee3b07044b1fecb9bddb
Gilbert <gilbertvelez96@github.com>
2020-06-16 16:48:52 -0400

    Seed file updated - GV

--- script/seed.js
+++ script/seed.js
@@ -1,18 +1,40 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Cart, Product} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
-  console.log('db synced!')
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+  await User.create({
+    name: 'John Doe',
+    email: 'johndoe@protonmail.com',
+    password: '123456789',
+    address: '123 Broadway, New York, NY 10001'
+  })
 
-  console.log(`seeded ${users.length} users`)
+  await Product.create({
+    title: 'Oak Chair',
+    description: 'A premium chair made from oak wood. Strong and sturdy',
+    price: 250.0,
+    quantity: 100,
+    type: 'chair',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  })
+
+  await Cart.create({
+    products: [
+      {
+        productId: 1,
+        price: 250.0,
+        quantity: 2
+      }
+    ],
+    checkedOut: false
+  })
+
+  // console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
 }
 
