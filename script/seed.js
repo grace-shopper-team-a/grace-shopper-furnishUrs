3a3541ef13a6d20c2e9bb9b8ede3f35d680cbd36
Ousainu Jabbi <ousainujabbi@gmail.com>
2020-06-17 17:21:47 -0400

    added more data to seed file, modified products model

--- script/seed.js
+++ script/seed.js
@@ -3,6 +3,149 @@
 const db = require('../server/db')
 const {User, Cart, Product} = require('../server/db/models')
 
+const arrayOfProducts = [
+  {
+    name: 'Oak Chair',
+    description: 'A premium chair made from oak wood. Strong and sturdy',
+    price: 250,
+    quantity: 100,
+    category: 'chair',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'LACK',
+    description: 'Ut imperdiet bibendum neque volutpat dapibus',
+    price: 100,
+    quantity: 100,
+    category: 'chair',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'NORDEN',
+    description:
+      'Ut pulvinar, nulla sit amet placerat suscipit, leo risus rutrum odio',
+    price: 80,
+    quantity: 100,
+    category: 'chair',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'JOKKMOKK',
+    description: 'Etiam egestas elit lectus, at elementum neque elementum at',
+    price: 300,
+    quantity: 100,
+    category: 'chair',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'LERHAMN',
+    description: 'Pellentesque quis facilisis lacus',
+    price: 50,
+    quantity: 100,
+    category: 'chair',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'LISABO',
+    description:
+      ' Duis ultricies, lorem at pharetra tempus, ipsum dui gravida enim, sit amet suscipit sapien elit nec neque',
+    price: 95,
+    quantity: 100,
+    category: 'table',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'MALM',
+    description: 'Suspendisse malesuada porta enim iaculis interdum',
+    price: 120,
+    quantity: 100,
+    category: 'table',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'GLADOM',
+    description:
+      'In laoreet massa nec mauris egestas, id fringilla nisi pretium. Ut et vestibulum ante.',
+    price: 75,
+    quantity: 100,
+    category: 'table',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'KOPPANG',
+    description:
+      'Phasellus ornare lorem nec tortor varius, non pellentesque erat iaculis',
+    price: 130,
+    quantity: 100,
+    category: 'table',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'HEMNES',
+    description: 'Donec imperdiet tincidunt erat id consectetur',
+    price: 150,
+    quantity: 100,
+    category: 'table',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'KULLEN',
+    description: 'Maecenas vulputate ipsum felis, ut auctor purus mattis a',
+    price: 180,
+    quantity: 100,
+    category: 'couch',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'KOPPANG',
+    description: 'Maecenas mattis consequat lacus nec tincidunt',
+    price: 110,
+    quantity: 100,
+    category: 'couch',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'NORDMELA',
+    description: 'Etiam blandit sapien quis magna maximus',
+    price: 99,
+    quantity: 100,
+    category: 'couch',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'LENNART',
+    description:
+      'Vestibulum porta, sem id auctor auctor, erat lorem tempus nunc',
+    price: 85,
+    quantity: 100,
+    category: 'couch',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  },
+  {
+    name: 'MICKE',
+    description: 'Proin tincidunt mattis maximus',
+    price: 125,
+    quantity: 100,
+    category: 'couch',
+    imageUrl:
+      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
+  }
+]
+
 async function seed() {
   await db.sync({force: true})
 
@@ -13,15 +156,11 @@ async function seed() {
     address: '123 Broadway, New York, NY 10001'
   })
 
-  await Product.create({
-    name: 'Oak Chair',
-    description: 'A premium chair made from oak wood. Strong and sturdy',
-    price: 250.0,
-    quantity: 100,
-    type: 'chair',
-    imageUrl:
-      'https://www.serenaandlily.com/dw/image/v2/AASD_PRD/on/demandware.static/-/Sites-masterCatalog_SerenaandLily/default/dwfea0d498/hi-res/Furn_Tucker_Chair_Natural_Oak_Angle_MV_0081_Crop_SH.jpg?sw=100&sh=125&sm=fit'
-  })
+  await Promise.all(
+    arrayOfProducts.map(product => {
+      return Product.create(product)
+    })
+  )
 
   await Cart.create({
     products: [
--- server/db/models/products.js
+++ server/db/models/products.js
@@ -1,32 +1,31 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require('sequelize')
+const db = require('../db')
 
 const Product = db.define('product', {
   name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-          notEmpty: true
-      }
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   description: {
-      type: Sequelize.TEXT
+    type: Sequelize.TEXT
   },
   price: {
-      type: Sequelize.DECIMAL,
-      validate: {
-          isDecimal: true
-      }
+    type: Sequelize.INTEGER,
+    allowNull: false
   },
   imageUrl: {
-      type: Sequelize.STRING,
-      defaultValue: 'https://cdn.vox-cdn.com/thumbor/hYG_03H7uW13XhJ5khtpB7N9fXI=/0x0:3600x2400/1200x800/filters:focal(1600x898:2176x1474)/cdn.vox-cdn.com/uploads/chorus_image/image/66549371/003_photo__8024407.0.jpg'
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://cdn.vox-cdn.com/thumbor/hYG_03H7uW13XhJ5khtpB7N9fXI=/0x0:3600x2400/1200x800/filters:focal(1600x898:2176x1474)/cdn.vox-cdn.com/uploads/chorus_image/image/66549371/003_photo__8024407.0.jpg'
   },
   category: {
-      type: Sequelize.ENUM('chair', 'table', 'couch', 'bed', 'drawers')
+    type: Sequelize.ENUM('chair', 'table', 'couch', 'bed', 'drawers')
   },
   quantity: {
-      type: Sequelize.INTEGER
+    type: Sequelize.INTEGER
   }
 })
 
